816302eab107daeb694ad1ffc4697f977f3b7b93
Lucas Louvem <120749192+LucasLouvem@users.noreply.github.com>
2025-10-29 16:16:28 +0000

    Finalizar retorno do endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ Chave: "valor" });
-}
+  const updateAt = new Date().toISOString();
+
+  const postgres_Version = await database.query("SHOW server_version;");
+  const databaseVersion = postgres_Version.rows[0].server_version;
+
+  const max_Connections = await database.query("SHOW max_connections;");
+  const databaseMaxConnections = max_Connections.rows[0].max_connections;
+
+  const databaseDB = process.env.POSTGRES_DB;
+  const databaseMaxConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;", 
+    values: [databaseDB]
+  })
+  const databaseOpenConnectionsValue = databaseMaxConnectionsResult.rows[0].count
+  
+  response.status(200).json({
+    update_at: updateAt,
+    version: parseInt(databaseVersion),
+    max_connections: parseInt(databaseMaxConnections),
+    opened_connections: databaseOpenConnectionsValue,
+  });
+  }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+});
+
+test("Get to Postgres Version, Max connections and Connections Used on /status", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.version).toEqual(16.0);
+  expect(responseBody.max_connections).toEqual(100);
+  expect(responseBody.opened_connections).toEqual(1);
 });
