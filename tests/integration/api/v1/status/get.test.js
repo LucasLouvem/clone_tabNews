b836f0c5f056552d8cfc22b4b9656c5ca49ce22d
Lucas Louvem <120749192+LucasLouvem@users.noreply.github.com>
2025-10-30 01:26:54 +0000

    makes  and the  more robust

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,13 +7,7 @@ test("get to /api/v1/status should return 200", async () => {
 
   const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(parsedUpdateAt);
-});
 
-test("Get to Postgres Version, Max connections and Connections Used on /status", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
-
-  const responseBody = await response.json();
   expect(responseBody.version).toEqual(16.0);
   expect(responseBody.max_connections).toEqual(100);
   expect(responseBody.opened_connections).toEqual(1);
