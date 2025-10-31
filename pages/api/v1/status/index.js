cffb06e8ba4f01955fd6278dfe6c210223dc818b
Lucas Louvem <120749192+LucasLouvem@users.noreply.github.com>
2025-10-31 16:56:22 +0000

    randomise database with SSL

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -11,17 +11,18 @@ async function status(request, response) {
 
   const databaseDB = process.env.POSTGRES_DB;
   const databaseMaxConnectionsResult = await database.query({
-    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;", 
-    values: [databaseDB]
-  })
-  const databaseOpenConnectionsValue = databaseMaxConnectionsResult.rows[0].count
-  
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseDB],
+  });
+  const databaseOpenConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].count;
+
   response.status(200).json({
     update_at: updateAt,
     version: parseInt(databaseVersion),
     max_connections: parseInt(databaseMaxConnections),
     opened_connections: databaseOpenConnectionsValue,
   });
-  }
+}
 
 export default status;
