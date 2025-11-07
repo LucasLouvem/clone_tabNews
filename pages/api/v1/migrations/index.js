b104b843d317ba33fcbf3fe499df65b44477a4c6
Lucas Louvem <120749192+LucasLouvem@users.noreply.github.com>
2025-11-07 00:42:54 +0000

    make the `migrations`end point more robust

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -13,13 +13,12 @@ export default async function migrations(request, response) {
     verbose: true,
     migrationsTable: "pgmigrations",
   };
+
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
     await dbClient.end();
     return response.status(200).json(pendingMigrations);
-  }
-
-  if (request.method === "POST") {
+  }else if (request.method === "POST") {
     const migratedMigrations = await migrationRunner({
       ...defaultMigrationsOptions,
       dryRun: false,
@@ -32,7 +31,10 @@ export default async function migrations(request, response) {
     }
 
     response.status(200).json(migratedMigrations);
-  }
 
-  return response.status(405).end();
+  }else{
+    await dbClient.end()
+    return response.status(405).end();
+  }
+  
 }
